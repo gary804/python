ef11c68989da96ac6a2ee13fe27be0959f90d686
gary804 <gary.guolin.jin@gmail.com>
2015-08-17 11:05:40 -0700

    update array_operation.py to add mergeSort() funtion and modify parameters definitions in def

--- array_operation.py
+++ array_operation.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 
-def hasDuplicate(*array):
+#def hasDuplicate(*array):
+def hasDuplicate(array=[]):
 	print array	#it become Tuple
 	for i in range(len(array)):
 		for j in range(i+1,len(array)):
@@ -10,9 +11,11 @@ def hasDuplicate(*array):
 
 #a = [1,2,3,8,4,5,6,7,8,9]	# works
 a = (0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,17)	# works
-print hasDuplicate(*a)
+#print hasDuplicate(*a)	#for def hasDuplicate(*array):
+print hasDuplicate(a)
 
-def binary_search(ai, *array):
+#def binary_search(ai, *array):
+def binary_search(ai, array=[]):
 	print array, ai
 	al = len(array)
 	p = al/2
@@ -50,7 +53,8 @@ def binary_search(ai, *array):
 							return "Index is: " + repr(hl1+j)
 					return "Index is: -1"
 
-def binary_search1(ai, *array):
+#def binary_search1(ai, *array):	#it works, but not good, the *array is for variable arguments
+def binary_search1(ai, array=[]):
 	print array, ai
 	al = len(array)
 	p = al/2
@@ -92,8 +96,10 @@ def binary_search1(ai, *array):
 
 a = list(a)
 a.sort()
+print '====================================================================='
 for i in range(-1,len(a)+2):
-	print binary_search(i, *a)
+	print binary_search(i, a)
+print '====================================================================='
 
 def atoi(str):
 	digit = {'0':0, '1':1,'2':2,'3':3,'4':4,'5':5,'6':6,'7':7,'8':8,'9':9}
@@ -147,3 +153,91 @@ def reverse_words_characters(string):
 	return ' '.join(wlist)
 
 print reverse_words_characters(string)
+
+def merge(arg1=[], arg2=[]):	#works, its default is the empty list
+#def merge(arg1, arg2):	#works
+	l = len(arg1)+len(arg2);
+	i1=0; i2=0;
+	list1 = [];
+	for i in range(l):
+		if (i1<len(arg1) and i2<len(arg2)):
+			if (arg1[i1]<arg2[i2]):
+				list1.append(arg1[i1])
+				i1+=1
+			else:
+				list1.append(arg2[i2])
+				i2+=1
+		elif (i1<len(arg1)):
+			list1.append(arg1[i1])
+			i1+=1
+		else:
+			list1.append(arg2[i2])
+			i2+=1
+	return list1
+print "mergeTesting:"
+l1=[1,3,5,7,9]
+l2= [0,2,4,6,8]
+l3 = merge(l1, l2)
+print l3
+
+def mergeSort(array=[]):
+	if len(array)==1:
+		return array
+	l = len(array)/2
+	a1 = mergeSort(array[0:l])
+	a2 = mergeSort(array[l:])
+	a3 = merge(a1, a2)
+	return a3
+
+def mergeSortIterate(array=[]):
+	k =1
+	while(k<=len(array)/2):
+		i=0
+		#print array, "k=", k
+		while(i<len(array)):
+			if i+k >=len(array):
+				break
+			wend=i+2*k
+			if wend>len(array):
+				wend= len(array)
+			a1 = array[i:i+k]
+			a2 = array[i+k:wend]
+			a3 = merge(a1, a2)
+			for j in range(wend-i):
+				array[i+j]=a3[j]
+
+			i=i+2*k
+		k = k*2
+		#print array
+	a1 = array[:k]
+	a2 = array[k:]
+	a3 = merge(a1,a2)
+	return a3
+print "mergeSortTesting:"
+l4 = [0,8,2,10,6,14,4,12,1,11,9,3,15,7,5,13]
+l5 = mergeSortIterate(l4)
+print l5,l4
+l4 = [0,8,2,6,10,14,4,12,1,11,9,3,7,5,13]
+l5 = mergeSortIterate(l4)
+print l5,l4
+l4 = [0,8,2,6,4,10,12,1,11,9,3,7,5,13]
+l5 = mergeSortIterate(l4)
+print l5,l4
+l4 = [0,8,2,6,4,12,10,1,11,9,3,7,5]
+l5 = mergeSortIterate(l4)
+print l5,l4
+l4 = [0,8,2,6,4,1,11,9,10,3,7,5]
+l5 = mergeSortIterate(l4)
+print l5,l4
+l4 = [0,8,2,6,4,1,9,3,7,5,10]
+l5 = mergeSortIterate(l4)
+print l5,l4
+l4 = [0,8,2,6,4,1,9,3,7]
+l5 = mergeSortIterate(l4)
+print l5,l4
+l4 = [0,8,2,6,4,1,9,3,]
+l5 = mergeSortIterate(l4)
+print l5,l4
+l4 = [0,8,2,6,4,1,9]
+l5 = mergeSortIterate(l4)
+print l5,l4
